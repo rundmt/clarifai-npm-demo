43ccc952f1ce741ffef6eb93b1c6a3ceeee39eb9
Derek Tor <derekmuoktor@gmail.com>
2016-04-26 08:23:43 -0700

    Added comments for demo

---
+++ clarifai-demo.js
@@ -0,0 +1,5 @@
+var Clarifai = require('./index.js');
+
+var c = new Clarifai('id','secret');
+//Check to see if function exported properly
+//console.log(c);
---
+++ index.js
@@ -0,0 +1,7 @@
+//Step 1
+function Clarifai(clientId, clientSecret){
+  this.clientId = clientId;
+  this.clientSecret = clientSecret;
+}
+
+module.exports = Clarifai;
