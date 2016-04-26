feb9a2ffdd789056e308cfe7808b1d228f677c3a
Derek Tor <derekmuoktor@gmail.com>
2016-04-26 08:31:26 -0700

    added access token method

--- clarifai-demo.js
+++ clarifai-demo.js
@@ -3,3 +3,9 @@ var Clarifai = require('./index.js');
 var c = new Clarifai('id','secret');
 //Check to see if function exported properly
 //console.log(c);
+
+//Step 3
+c.getAccessToken(function(err, res){
+  // Check to see if access_token comes through properly.
+  // console.log(res.body.access_token);
+});
--- index.js
+++ index.js
@@ -7,4 +7,19 @@ function Clarifai(clientId, clientSecret){
   this.clientSecret = clientSecret;
 }
 
+//Step 3
+Clarifai.prototype.getAccessToken = function(cb) {
+  var _this = this;
+  request
+    .post('https://api.clarifai.com/v1/token/')
+    .send('client_id='+this.clientId)
+    .send('client_secret='+this.clientSecret)
+    .send('grant_type=client_credentials')
+    .set('Content-Type', 'application/x-www-form-urlencoded')
+    .end(function(err, res){
+      _this.accessToken = res.body.access_token;
+      cb(err, res);
+    });
+};
+
 module.exports = Clarifai;
